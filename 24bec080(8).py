05f6b82bfb45157bbc7479b8a23bb53d3009030f
Mayani om <mayaniom7@gmail.com>
2025-01-21 17:42:17 +0530

    Add files via upload

---
+++ 24bec080(8).py
@@ -0,0 +1,7 @@
+a=int(input("enter the degree a:"))
+b=int(input("enter the degeree b:"))
+c=int(input("enter the degree c:"))
+if(a+b+c==180):
+    print("your triangle is valid",a+b+c)
+else:
+    print("your triangle is not valid",a+b+c)
